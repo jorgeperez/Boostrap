0bccd47d551cbd2c615766d2a49b55c257db8666
jorgeperez <jorgeprz18@gmail.com>
2015-06-11 19:54:24 -0500

    Estructura de la cabecera

---
+++ detail.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<?php include 'inc/head_common.php';?>
+	<title>Workshop Primero - Detalles</title>
+	<!-- Latest compiled and minified CSS -->
+<!-- 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	
+ --></head>
+<body>
+	<?php include 'inc/header.php';?>
+	<p>Boostrap ejemplo</p>
+	 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	// Latest compiled and minified JavaScript
+	// <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> -->
+	<?php include 'inc/footer_common.php';?>
+</body>
+</html>
